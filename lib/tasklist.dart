4e588909e403202c8a8f1ec6628f697c0ae35376
ThanGon <nate.goncalves@gmail.com>
2022-03-04 14:33:37 -0300

    latest

--- lib/tasklist.dart
+++ lib/tasklist.dart
@@ -1,32 +1,43 @@
 import 'package:flutter/material.dart';
 
+class TaskList extends StatelessWidget {
+  TaskList({Key? key}) : super(key: key);
 
-class TaskList extends StatefulWidget {
-  const TaskList({ Key? key }) : super(key: key);
+  final TextEditingController emailController = TextEditingController();
 
-  @override
-  State<TaskList> createState() => _TaskListState();
-}
-
-class _TaskListState extends State<TaskList> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: const Center(
+      body: Center(
         child: Padding(
-          padding: EdgeInsets.symmetric(horizontal: 16), 
-          child: TextField(
-            decoration: InputDecoration(
-              labelText: 'E-Mail',
-              hintText: 'exemplo@exemplo.com',
-              border: OutlineInputBorder(),
-              prefixText: '' // Argumento para um prefixo predefinido na caixa de texto
+          padding: const EdgeInsets.symmetric(horizontal: 16),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+            TextField(
+              controller: emailController,
+              decoration: const InputDecoration(
+                labelText: 'E-Mail',
+                hintText: 'exemplo@exemplo.com',
+                border: OutlineInputBorder(),
+                prefixText: '', // Argumento para um prefixo predefinido na caixa de texto
+              ),
+              obscureText: false, // Argumento para censurar o texto como senha
+              keyboardType: TextInputType.emailAddress, // Argumento para trocar o tipo de teclado no dispositivo
             ),
-            obscureText: false, // Argumento para censurar o texto como senha
-            keyboardType: TextInputType.emailAddress, // Argumento para trocar o tipo de teclado no dispositivo
-          )
+            ElevatedButton(
+              onPressed: login,
+              child: const Text('Entrar'),
+            ),
+          ]),
         ),
       ),
     );
+  }
+
+  void login() {
+    String feedback = emailController.text;
+    print(feedback);
+
   }
 }
