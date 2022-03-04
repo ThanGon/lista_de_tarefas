30c8f8226ea34300a0fc0fe3a53ef6102815d11f
ThanGon <nate.goncalves@gmail.com>
2022-03-04 15:31:17 -0300

    latest

--- lib/tasklist.dart
+++ lib/tasklist.dart
@@ -1,12 +1,36 @@
 import 'package:flutter/material.dart';
 
 class TaskList extends StatelessWidget {
-  const TaskList({ Key? key }) : super(key: key);
+  const TaskList({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Scaffold(
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(12.0),
+          child: Row(
+            children: [
+              Expanded( // Expanded widget que ocupa o espaço inteiro da tela
+                child: TextField(
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Adicione uma tarefa',
+                  ),
+                ),
+              ),
+              SizedBox(width: 9,),
+              ElevatedButton(
+                onPressed: null,
+                child: Text('+'),
+                style: ElevatedButton.styleFrom(
+
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
